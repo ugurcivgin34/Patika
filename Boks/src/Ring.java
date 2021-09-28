b5a542628c4a6d98895ceeb29fda86214d9178d4
Uğur Okan Çivgin <37384522+ugurcivgin34@users.noreply.github.com>
2021-09-28 20:06:28 +0300

    BoksOyunu

---
+++ Boks/src/Ring.java
@@ -0,0 +1,74 @@
+public class Ring {
+    Fighter f1;
+    Fighter f2;
+    int minWeight;
+    int maxWeight;
+
+    public Ring(Fighter f1, Fighter f2, int minWeight, int maxWeight) {
+        this.f1 = f1;
+        this.f2 = f2;
+        this.minWeight = minWeight;
+        this.maxWeight = maxWeight;
+    }
+
+    
+    public int Sans() {
+        double randomValue = Math.random() * 100;  //0.0 to 99.9
+        return (int) randomValue;
+    }
+    
+    public void run() {
+
+        if (checkWeight()) {
+            while (f1.health > 0 && f2.health > 0) {
+            	
+            
+                System.out.println("======== YENÝ ROUND ===========");
+                
+               if(Sans()<=50) {
+            	   f2.health = f1.hit(f2);
+                   if (isWin()){
+                       break;
+                   }
+               } else {
+            	   f1.health = f2.hit(f1);
+                   if (isWin()){
+                       break;
+                   }
+                   
+               }
+               printScore();
+                
+                
+            }
+
+        } else {
+            System.out.println("Sporcularýn aðýrlýklarý uyuþmuyor.");
+        }
+
+
+    }
+
+    public boolean checkWeight() {
+        return (f1.weight >= minWeight && f1.weight <= maxWeight) && (f2.weight >= minWeight && f2.weight <= maxWeight);
+    }
+
+    public boolean isWin() {
+        if (f1.health == 0) {
+            System.out.println("Maçý Kazanan : " + f2.name);
+            return true;
+        } else if (f2.health == 0){
+            System.out.println("Maçý Kazanan : " + f1.name);
+            return true;
+        }
+
+        return false;
+    }
+
+
+    public void printScore() {
+        System.out.println("------------");
+        System.out.println(f1.name + " Kalan Can \t:" + f1.health);
+        System.out.println(f2.name + " Kalan Can \t:" + f2.health);
+    }
+}
